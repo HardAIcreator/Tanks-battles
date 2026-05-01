779b2e605abb52bf5e0b864bd04fb726202a7af3
Hard <rguncevskij@gmail.com>
2026-05-01 18:54:10 +0500

    Refactor service worker to use new cache version

--- sw.js
+++ sw.js
@@ -1,34 +1,18 @@
-const CACHE_NAME = 'tanks-v2';
-
-// Упрощаем список до минимума
-const assets = [
-  'index.html',
-  'manifest.json',
-  'icon.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      // Используем addAll, но если что-то пойдет не так, SW не умрет
-      return cache.addAll(assets).catch(err => console.log('Кэш пока не собран'));
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('tanks-v1').then((cache) => {
+      return cache.addAll([
+        '/Tanks-battles/',
+        '/Tanks-battles/index.html',
+        '/Tanks-battles/manifest.json',
+        '/Tanks-battles/icon.png'
+      ]).catch(err => console.log('Ошибка кэширования'));
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
-});
-
-// Активация нового воркера и удаление старого кэша
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      );
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });

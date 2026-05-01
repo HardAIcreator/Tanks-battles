5a486bee16c27855003054b0785915b1566e4022
Hard <rguncevskij@gmail.com>
2026-05-01 18:38:18 +0500

    Update service worker to use new cache name

--- sw.js
+++ sw.js
@@ -1,16 +1,20 @@
+const CACHE_NAME = 'tanks-v1';
+
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('tanks-store').then((cache) => cache.addAll([
-      '/Tanks-battles/',
-      '/Tanks-battles/index.html',
-      '/Tanks-battles/manifest.json',
-      '/Tanks-battles/icon.png'
-    ]))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        './',
+        'index.html',
+        'manifest.json',
+        'icon.png'
+      ]).catch(err => console.log('Кэширование не удалось:', err));
+    })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });

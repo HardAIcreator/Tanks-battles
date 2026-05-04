3d32d91efee31e7d11902470faf9f669d13f20cf
Hard <rguncevskij@gmail.com>
2026-05-04 20:01:38 +0500

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,26 +1,18 @@
-const CACHE_NAME = 'tanks-v3';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.png',
-];
-
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open('tanks-v1').then((cache) => {
+      return cache.addAll([
+        '/Tanks-battles/',
+        '/Tanks-battles/index.html',
+        '/Tanks-battles/manifest.json',
+        '/Tanks-battles/icon.png'
+      ]).catch(err => console.log('Ошибка кэширования'));
+    })
   );
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request).catch(() => caches.match('./index.html'));
-    })
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
